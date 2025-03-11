c182b3671495cffdda1d253523d62e03ffc3c42f
alvinirudayarajan <163723809+allwinrajan@users.noreply.github.com>
2025-03-11 12:06:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          We Are the Devops Team
-          Alvin & Basith
+          Alvin & Basith &
         </p>
         <a
           className="App-link"
